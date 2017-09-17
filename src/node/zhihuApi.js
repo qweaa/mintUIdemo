8bfe38eab7822bdfb0dfbb81f86f20977694c2bd
qweaa <2275414653@qq.com>
2017-09-18 07:41:43 +0800

    使用node请求数据

--- src/node/zhihuApi.js
+++ src/node/zhihuApi.js
@@ -15,25 +15,42 @@ let username = 'shanejs';
 
 http.createServer(function (request, response) {
 
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8',
-        'Access-Control-Allow-Origin':"*"
-    });
-    
-    let params = url.parse(request.url, true).query;
-    if(params.m === "zhuanlan"){
-        zhihu.Post.page('bigertech').then(function (list) {
-            response.end(JSON.stringify(list));
-        });
-    }else{
-        response.end("缺少参数m或m值错误！");
-    }
+            // 发送 HTTP 头部 
+            // HTTP 状态值: 200 : OK
+            // 内容类型: text/plain
+            response.writeHead(200, {
+                'Content-Type': 'text/plain; charset=utf-8',
+                'Access-Control-Allow-Origin': "*"
+            });
 
-    
-}).listen(8888);
+            let params = url.parse(request.url, true).query;
+            if (params.m === "zhuanlan") {
+                zhihu.Post.page('koukou', {
+                    limit: params.limit,
+                    offset: params.offset
+                }).then(function (list) {
+                    response.end(JSON.stringify(list));
+                });
+            }else if (params.m === "default") {
+                http.get(reqUrl, (sres) => {
+                    if (sres.statusCode !== 200) {
+                        // throw error
+                    }
+                    var rawData = '';
+                    sres.setEncodeng('binary');
+                    sres.on('data', (chunk) => {
+                        rawData += chunk;
+                    })
+                    sres.on('end', () => {
+                        // callback here
+                    })
+                })
+            }else {
+                response.end("缺少参数m或m值错误！");
+            }
 
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+
+            }).listen(8888);
+
+        // 终端打印如下信息
+        console.log('Server running at http://127.0.0.1:8888/');
--- src/views/index.vue
+++ src/views/index.vue
@@ -6,7 +6,8 @@
 .mint-loadmore-content ul li {
     border-bottom: 1px solid #ccc;
 }
-.mint-loadmore-content ul li img{
+
+.mint-loadmore-content ul li img {
     border-radius: 50%;
 }
 
@@ -28,26 +29,34 @@
     display: inline-block;
     color: #26a2ff;
 }
+
+.showNone {
+    text-align: center;
+    color: #666;
+    line-height: 40px;
+    font-size: 12px;
+}
 </style>
 
 
 <template>
     <div class="app">
         <v-header :title="title"></v-header>
-        <mt-loadmore :top-method="loadTop" :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" @top-status-change="handleTopChange">
-            <ul v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="10">
-                <li v-for="(item, index) in list" :key="index">
-                    <mt-cell :title="item.title" to="/article" is-link value="">
-                        <img :src="item.url" alt="">
-                    </mt-cell>
-                </li>
-            </ul>
-            <div class="loadingAnimate">
-                <mt-spinner type="triple-bounce" v-show="loading" color="#26a2ff"></mt-spinner>
-            </div>
+        <mt-loadmore @load="initInfo()" :top-method="loadTop" :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" @top-status-change="handleTopChange">
             <div slot="top" class="mint-loadmore-top">
                 <mt-spinner type="snake" color="#26a2ff" v-show="topStatus !== 'loading'" :class="{ 'rotate': topStatus === 'drop' }"></mt-spinner>
                 <span v-show="topStatus === 'loading'">Loading...</span>
+            </div>
+            <ul v-infinite-scroll="loadMore" :infinite-scroll-disabled="!loading" infinite-scroll-distance="20">
+                <li v-for="(item, index) in list" :key="index">
+                    <mt-cell :title="item.title" to="/article" is-link value="">
+                        <img :src="'http://pic4.zhimg.com/'+item.author.avatar.id+'_s.jpg'" alt="">
+                    </mt-cell>
+                </li>
+            </ul>
+            <div class="showNone" v-show="showNone">木有了...</div>
+            <div class="loadingAnimate">
+                <mt-spinner type="triple-bounce" v-show="loading" color="#26a2ff"></mt-spinner>
             </div>
         </mt-loadmore>
         <!-- <v-footer></v-footer> -->
@@ -55,15 +64,19 @@
 </template>
 
 <script>
-import Header from './components/header'
+import Header from './components/header';
+import axios from "axios";
 export default {
     data() {
         return {
-            list: null,
+            list: [],
             topStatus: '',
             allLoaded: false,
             loading: false,
-            title: "专栏"
+            title: "专栏",
+            limit: 20,
+            offset: 0,
+            showNone: false
         }
     },
     methods: {
@@ -82,33 +95,54 @@ export default {
             location.reload();
         },
         loadMore() {
-            this.loading = true;
-            axios.get("http://127.0.0.1:8888?m=zhuanlan").then((data) => {
+            if (!this.showNone) {
+                this.loading = true;
+                axios.get("http://127.0.0.1:8888?m=zhuanlan&limit=" + this.limit + "&offset=" + this.offset).then((data) => {
+                    if (data.data.length === 0) {
+                        this.loading = false;
+                        this.showNone = true;
+                    } else {
+                        if (this.list.length === 0) {
+                            this.list = data.data;
+                            this.offset += this.limit;
+                            this.limit = 10;
+                            this.loading = false;
+                        } else {
+                            this.list = [...this.list, ...data.data];
+                            this.offset += this.limit;
+                            this.loading = false;
+                        }
+                    }
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }
+
+        },
+        initInfo() {
+            axios.get("http://127.0.0.1:8888?m=default").then((data) => {
                 console.log(data)
                 this.list = data.data;
-                console.log(this.list)
             }).catch((err) => {
                 console.log(err)
             })
-            // setTimeout(() => {
-            //     let last = this.list[this.list.length - 1];
-            //     for (let i = 1; i <= 10; i++) {
-            //         this.list.push(last + i);
-            //     }
-            //     this.loading = false;
-            // }, 2500);
         }
     },
     components: {
         vHeader: Header
     }
 };
-import axios from "axios";
-// axios.get("http://127.0.0.1:8888?m=zhuanlan").then((data) => {
-//     console.log(data)
-//     Vue.list = data;
-// }).catch((err) => {
-//     console.log(err)
-// })
+
+function jsonpCallback(result) {
+    console.log(result)
+}
+var JSONP = document.createElement("script");
+JSONP.type = "text/javascript";
+JSONP.id = "aaa"
+JSONP.src = "https://zhuanlan.zhihu.com/api/recommendations/columns?limit=10&offset=0&seed=46&call=jsonpCallback";
+document.getElementsByTagName("head")[0].appendChild(JSONP);
+
+let aaa = document.getElementById("aaa");
+console.log(window);
 
 </script>
